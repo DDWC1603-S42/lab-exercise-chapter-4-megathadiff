cdb4d11559b8526717d0aa53751e7a66f01adde2
megathadiff <35548181+megathadiff@users.noreply.github.com>
2018-01-21 18:43:31 +0800

    seeyap

--- Lab-Exercise-3.cpp
+++ Lab-Exercise-3.cpp
@@ -2,14 +2,25 @@
 //fix and complete the program below.
 
 
-cout<<"user input a number: ";
-
-for(t=num;num!=0;num=num/10)
+#include <iostream>
+using namespace std;
+int main()
 {
+
+	int r,sum=0,t;
+	int num;
+
+	cout<<"user input a number: "<<num<<endl;
+	cin>>num;
+
+	for(t=num;num!=0;num=num/10)
+	{
 	r=num % 10;
 	sum=sum*10+r;
 
-}
-cout<<"The number in reverse order: "<<endl;
+	}
+cout<<"The number in reverse order is: "<<sum;
+
+return 0;
 
 }
